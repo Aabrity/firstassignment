beaca1eae83f8c0a0cd11839f884e6932e903eef
Aabrity <aabrityd@gmail.com>
2024-11-26 16:05:47 +0545

    first commit (main base)

---
+++ first_assignment/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'view/simple_interest_screen.dart';
+import 'view/area_of_circle_screen.dart';
+import 'view/arithmetic_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Dashboard',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: DashboardScreen(),
+    );
+  }
+}
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SimpleInterestScreen()),
+                );
+              },
+              child: Text('Simple Interest'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AreaOfCircleScreen()),
+                );
+              },
+              child: Text('Area of Circle'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ArithmeticScreen()),
+                );
+              },
+              child: Text('Arithmetic'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
